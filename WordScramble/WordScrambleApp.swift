f10c59c30658b9d46543d2c12c7b9f508bba2ea7
Devy <izabela.marcinkowska@outlook.com>
2024-09-10 14:21:41 +0200

    First commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Izabela Marcinkowska on 2024-09-10.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
